785a658e59f45da6deba267708672cc9784886fc
AslanBolurov <aslanbolurov@list.ru>
2023-11-15 11:59:45 +0300

    3 @RequestMapping

--- src/main/kotlin/com/aslanjavasky/hogwartslibrary/controller/LibraryController.kt
+++ src/main/kotlin/com/aslanjavasky/hogwartslibrary/controller/LibraryController.kt
@@ -2,23 +2,27 @@ package com.aslanjavasky.hogwartslibrary.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 
 /**
  * @author Aslan Javasky, Java/Kotlin developer, Telegram messanger:@Aslan_Javasky
  */
 @Controller
+@RequestMapping("lib")
 class LibraryController {
 
     //GET, PUT, POST, DELETE, PATCH
-    @GetMapping("lib/welcome")
+    @GetMapping("/welcome")
+//    @RequestMapping(method = [RequestMethod.GET], value = ["lib/welcome"])
     fun welcome()= "hello"
 
 
-    @GetMapping("lib/regular")
+    @GetMapping("/regular") //lib/regular
     fun regular()="regular_section"
 
 
-    @GetMapping("lib/secret")
+    @GetMapping("/secret") //lib/secret
     fun secter()="secret_section"
 
 
