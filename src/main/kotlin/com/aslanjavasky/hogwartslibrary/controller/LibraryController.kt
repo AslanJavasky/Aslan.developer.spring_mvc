0d16a1c90d8176d9be42b36b0dbaf686ca9441de
AslanBolurov <aslanbolurov@list.ru>
2023-11-16 09:44:04 +0300

    4 HttpServletRequest

--- src/main/kotlin/com/aslanjavasky/hogwartslibrary/controller/LibraryController.kt
+++ src/main/kotlin/com/aslanjavasky/hogwartslibrary/controller/LibraryController.kt
@@ -1,9 +1,11 @@
 package com.aslanjavasky.hogwartslibrary.controller
 
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import java.util.logging.Logger
 
 /**
  * @author Aslan Javasky, Java/Kotlin developer, Telegram messanger:@Aslan_Javasky
@@ -19,7 +21,12 @@ class LibraryController {
 
 
     @GetMapping("/regular") //lib/regular
-    fun regular()="regular_section"
+    fun regular(request:HttpServletRequest): String {
+        val nameOfBook=request.getParameter("bookname")
+        val author=request.getParameter("author")
+        Logger.getLogger(LibraryController::class.java.name).info("The author:$author and name of Book is $nameOfBook")
+        return "regular_section"
+    }
 
 
     @GetMapping("/secret") //lib/secret
